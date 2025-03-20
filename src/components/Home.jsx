8b0e1b99d4f22ec980f767abdded21751d834eff
Devansh Chaturvedi <devanshchaturvedi273@gmail.com>
2025-03-20 08:59:55 +0530

    paste-app

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -9,53 +9,45 @@ const Home = () => {
   const [title, setTitle] = useState("");
   const [value, setValue] = useState("");
   const [searchParams, setsearchParams] = useSearchParams({});
-  const [isInput , setisInput ] = useState(true)
-
-  
+  const [isInput, setisInput] = useState(true);
 
   const pasteId = searchParams.get("pasteId");
-  
-  
-  const dispatch = useDispatch()
 
-  const allpastes = useSelector((state) => state.paste.pastes)
+  const dispatch = useDispatch();
 
- useEffect(() => {
+  const allpastes = useSelector((state) => state.paste.pastes);
 
-  if(pasteId){
-    const paste = allpastes.find((p) => p._id === pasteId)
-    setTitle(paste.title)
-    setValue(paste.content)
-  }
-    
- }, [pasteId])
- 
+  useEffect(() => {
+    if (pasteId) {
+      const paste = allpastes.find((p) => p._id === pasteId);
+      setTitle(paste.title);
+      setValue(paste.content);
+    }
+  }, [pasteId]);
 
-  function handleClick(){
-    if(value === ""){
-       setisInput(false)
-       toast("Please Enter the title")
-    }else{
+  function handleClick() {
+    if (value === "") {
+      setisInput(false);
+      toast("Please Enter the title");
+    } else {
       const paste = {
-        title : title,
-        content : value,
-        _id : pasteId ||  Date.now().toString(12),
-        createdAt : new Date().toISOString()
-      }
-      if (pasteId){
+        title: title,
+        content: value,
+        _id: pasteId || Date.now().toString(12),
+        createdAt: new Date().toISOString(),
+      };
+      if (pasteId) {
         //update
-        dispatch(updateToPaste(paste))
-      }else{
+        dispatch(updateToPaste(paste));
+      } else {
         //create
-        dispatch(addToPaste(paste))
+        dispatch(addToPaste(paste));
       }
-      setTitle('')
-      setValue('')
-      setsearchParams({})
+      setTitle("");
+      setValue("");
+      setsearchParams({});
     }
-      
-    }
-  
+  }
 
   return (
     <div>
@@ -67,27 +59,36 @@ const Home = () => {
           value={title}
           onChange={(e) => setTitle(e.target.value)}
         />
-       <button onClick={handleClick}  className="bg-blue-600 p-3 text-white font-semibold border rounded-xl transition-all transform duration-300 hover:bg-blue-900">
+        <button
+          onClick={handleClick}
+          className="bg-blue-600 p-3 text-white font-semibold border rounded-xl transition-all transform duration-300 hover:bg-blue-900"
+        >
           {pasteId ? "Update My Paste" : "Create My Paste"}
         </button>
       </div>
       <div className="relative  ml-[23.6rem] mt-[2rem]">
         <div className="">
-        <FaCircle className="absolute top-[33.8%] left-4 size-3 text-red-600"/>
-        <FaCircle className="absolute top-[33.8%] left-9 size-3 text-yellow-500"/>
-        <FaCircle className="absolute top-[33.8%] left-14 size-3 text-green-700"/>
-        <IoCopyOutline className="absolute top-[25%] left-[70rem] size-5" onClick={() => {
+          <FaCircle className="absolute top-[33.8%] left-4 size-3 text-red-600" />
+          <FaCircle className="absolute top-[33.8%] left-9 size-3 text-yellow-500" />
+          <FaCircle className="absolute top-[33.8%] left-14 size-3 text-green-700" />
+          <button
+            onClick={() => {
               navigator.clipboard.writeText(content);
               toast.success("Copy to clipboard");
-            }}/>
+            }}
+          >
+            <IoCopyOutline className="absolute top-[25%] left-[70rem] size-5" />
+          </button>
         </div>
-      
-      <input className="flex justify-center items-center bg-white h-[35px] border border-gray-500 w-[1160px] rounded-sm outline-none " disabled/>
-      
+
+        <input
+          className="flex justify-center items-center bg-white h-[35px] border border-gray-500 w-[1160px] rounded-sm outline-none "
+          disabled
+        />
       </div>
-     
+
       <div className="flex justify-center mr-[0.3rem]">
-      <textarea
+        <textarea
           className="bg-white w-[1160px] h-[500px] text-black p-3 border rounded-sm text-xl outline-blue-700 "
           value={value}
           placeholder="Enter text here..."
